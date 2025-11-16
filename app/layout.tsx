8e22c7ff8f506ea3cf59d025e57252c4a1c60ff2
teerapat-ch <coptee413@gmail.com>
2025-11-16 09:46:01 +0700

    adjust layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,16 @@
 import type { Metadata } from "next";
-import { Google_Sans_Code } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-
-const sansCode = Google_Sans_Code({
-  variable: "--font-sans-code",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Teerapat Ch. Portfolio",
   description: "Teerapat Ch. Portfolio",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
-      <body className={`${sansCode.variable} ${sansCode.variable} antialiased`}>
+    <html>
+      <body>
         <Navbar />
         {children}
       </body>
